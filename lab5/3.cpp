9174fa4fce6896c45f8c1c531d78648c8649bd98
Mian Saad Tahir <tahirsaad99@gmail.com>
2023-10-03 02:15:57 +0500

    Add files via upload

---
+++ lab5/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+main()
+{
+    float n, result;
+    cout<<"Enter a number: ";
+    cin>>n;
+    result=sqrt(n);
+    cout<<"The square root of "<<n<<" is: "<<result;
+
+}
