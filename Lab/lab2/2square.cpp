f297600ec892b3cc119a863e47cb63e98a497ba3
Mian Saad Tahir <tahirsaad99@gmail.com>
2024-02-25 20:14:13 +0500

    Add files via upload

---
+++ Lab/lab2/2square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "----------" << endl;
+    cout << "|        |" << endl
+         << "|        |" << endl
+         << "|        |" << endl;
+    cout << "----------" << endl;
+}
